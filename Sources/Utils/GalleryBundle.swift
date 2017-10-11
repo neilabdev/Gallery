60376b3d1b26ea3b1669d852f06aa2d0277350e8
ghost <ghost@neilab.com>
2017-10-11 14:06:17 -0400

    1. GallaryController now may be subclassed. 2. GallaryBundle successfully loads resources in development pod.

--- Sources/Utils/GalleryBundle.swift
+++ Sources/Utils/GalleryBundle.swift
@@ -1,9 +1,16 @@
 import UIKit
+import Foundation
 
 class GalleryBundle {
-
   static func image(_ named: String) -> UIImage? {
-    let bundle = Foundation.Bundle(for: Bundle.self)
-    return UIImage(named: "Gallery.bundle/\(named)", in: bundle, compatibleWith: nil)
+    let bundleForClass = Bundle(for: GalleryBundle.self)
+    let bundleURL = bundleForClass.resourceURL?.appendingPathComponent("Gallery.bundle")
+
+    if let url = bundleURL, let resourceBundle = Bundle(url: url) { // NOTE: Should always succeed
+      return UIImage(named: named, in: resourceBundle, compatibleWith: nil)
+    } else {
+      let bundle = Bundle(for: Bundle.self)
+      return UIImage(named: "Gallery.bundle/\(named)", in: bundle, compatibleWith: nil)
+    }
   }
 }
